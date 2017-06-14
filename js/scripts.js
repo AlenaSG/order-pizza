4fa9d00a59d8e9bee7e271ef43f9aa6d5f85fd0e
Alena Golovina and Josh Yunker <josh@>
2017-06-14 11:35:58 -0700

    random number generator for two players to the game

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,49 @@
 //business logic
-var price = 0;//set global vaiable
+//set global vaiable
 //Set Objects
-function Ticket(movie, time, age) {
-  this.movieType = movie;
-  this.timeOfDay = time;
-  this.ageRange = age;
+function Player(name, score, totals) {
+  this.playerName = name;
+  this.playerScore = score;
+  this.playerTotalScore = totals;
 }
+//var score = 0;
+var playerOne = new Player("Player1", 0);
+var playerTwo = new Player("Player2", 0);
 
 //Set Object Prototypes
-Ticket.prototype.deliveredPrice = function() {
-  if (this.movieType === "secondRun" && this.timeOfDay === "eveing" && this.ageRange <= 10) {
-    price = 10;
-  } else if (this.movieType === "secondRun" && this.timeOfDay === "evening" && this.ageRange < 65 && this.ageRange > 10) {
-    price = 15;
-  } else if (this.movieType === "secondRun" && this.timeOfDay === "evening" && this.ageRange > 64) {
-  price = 5;
+Player.prototype.totalScore = function() {
+  return (this.playerScore + showScore())
+};
+
+    //price = 10;
+  //} else if (this.movieType === "secondRun" && this.timeOfDay === "evening" && this.ageRange < 65 && this.ageRange > 10) {
+    //price = 15;
+  //} else if (this.movieType === "secondRun" && this.timeOfDay === "evening" && this.ageRange > 64) {
+  //price = 5;
+//}
+
+//if (this.movieType === "newRelease") {
+  //price = price + 5;
+//}//return price;
+//}
+function showScore() {
+return Math.floor((Math.random() * 6) + 1);
 }
 
-if (this.movieType === "newRelease") {
-  price = price + 5;
-}
-return price;
-}
-
-
+alert(this.playerScore + showScore());
 //User interface logic
 
 $(document).ready(function() {
-  $("form#ticket-prices").submit(function(event) {
-      event.preventDefault();
+  $("form#player1").submit(function(event){
 
-    var inputtedMovieType = $("input:radio[name=typeOfMovie]:checked").val();
-    var inputtedTimeOfDay = $("input:radio[name=timeOfDay]:checked").val();
-    var inputtedAges = parseInt($("input#ageNumber").val());
-
-    var newTicket = new Ticket(inputtedMovieType, inputtedTimeOfDay, inputtedAges);
-    newTicket.deliveredPrice();
-
-    $("ul#prices").text("$" + price);
-
-
+  var yourFirstScore = showScore();
+    $("ul#player1-score").text(yourFirstScore);
+    event.preventDefault();
   });
-});
+  $("form#player2").submit(function(event) {
+
+    var yourSecondScore = showScore();
+    $("ul#player2-score").text(yourSecondScore);
+    event.preventDefault();
+  });
+  });
