60b661f53483d802f73ef85dab720c6392fb3b94
AlenaSG <alenasgolovina@gmail.com>
2017-06-16 16:30:37 -0700

    add toppings as a part of the price calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,28 @@
-//BUSINESS LOGIC
-function Pizza(size) {
-  this.size = size;
-  //this.toppings = toppings;
-}
-
-Pizza.prototype.price = function () {
-  if (size === "small") {
-    price = 5;
-  } else if (size === "medium") {
-    price = 8;
-  } else {
-    price = 10;
-  }
-  return price;
-};
-
-function Clear() {
-$("input:radio[name=size]:checked").val("");
-$("input:checkbox[name=topping]:checked").val("");
-}
-
-
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
   $("form#order-pizza").submit(function(event) {
       event.preventDefault();
+      checkboxes();
+      $("input:checkbox[name=topping]:checked").each(function() {
+        var toppingMode = $(this).val();
+        $('#summary').append(toppingMode + "<br>");
+
+var toppingMode = $(this).val();
 
 
-
-    var newPizza = new Pizza(sizeResult);
+    var newPizza = new Pizza(sizeResult, toppingMode);
 
 var sizeResult = $("input:radio[name=size]:checked").val();
 $("#size").text(sizeResult);
-$("input:checkbox[name=topping]:checked").each(function(){
-  var toppingMode = $(this).val();
-  $('#summary').append(toppingMode + "<br>");
-  var priceResult = newPizza.price();
+
+
+finalPrice();
+
+  var priceResult = newPizza.finalPrice();
 $("#price").text(priceResult);
 $("#receipt").show();
 });
  $('#order-pizza').hide();
 });
 });
-//var number1 = parseInt($("#input1").val());
-//var number2 = parseInt($("#input2").val());
-//var operator = $("input:radio[name=operator]:checked").val();
