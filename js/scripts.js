8010a0c478e74014e1852de0611f1223901c02d9
AlenaSG <alenasgolovina@gmail.com>
2017-06-16 18:50:18 -0700

    update user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,48 @@
+//BUSINESS LOGIC
+function Pizza(pizzaSize, toppings) {
+  this.pizzaSize = pizzaSize;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.price = function () {
+  if (this.pizzaSize === "small") {
+    price = 5;
+  } else if (this.pizzaSize === "medium") {
+    price = 8;
+  } else if (this.pizzaSize === "large") {
+    price = 10;
+  } else {
+    alert("Please choose a size");
+  return 0;
+  }
+  if (this.toppings.length === 3) {
+    price += 2;
+  } else if (this.toppings.length > 3) {
+    price += 4;
+  }
+  return price;
+};
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
   $("form#order-pizza").submit(function(event) {
-      event.preventDefault();
-      checkboxes();
-      $("input:checkbox[name=topping]:checked").each(function() {
-        var toppingMode = $(this).val();
-        $('#summary').append(toppingMode + "<br>");
-
-var toppingMode = $(this).val();
-
-
-    var newPizza = new Pizza(sizeResult, toppingMode);
-
-var sizeResult = $("input:radio[name=size]:checked").val();
-$("#size").text(sizeResult);
-
-
-finalPrice();
-
-  var priceResult = newPizza.finalPrice();
-$("#price").text(priceResult);
-$("#receipt").show();
-});
- $('#order-pizza').hide();
-});
+    event.preventDefault();
+    toppings = [];
+    toppingsList = " with:";
+    $("input:checkbox[name=topping]:checked").each(function() {
+      var toppingsChoice = $(this).val();
+      toppingsList += "<br>" + toppingsChoice;
+      toppings.push(toppingsChoice);
+    });
+    if (toppings.length > 0) {
+      $('#summary').append(toppingsList);
+    }
+    var sizeResult = $("input:radio[name=size]:checked").val();
+    var newPizza = new Pizza(sizeResult, toppings);
+    var price = newPizza.price();
+    $("#size").text(sizeResult);
+    $("#price").text(price);
+    $("#receipt").show();
+    $('#order-pizza').hide();
+  });
 });
